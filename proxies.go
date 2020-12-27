4e0c6be155b5f55b6081e0beca72ce9064c5a31d
dz <denis.zarubin@gmail.com>
2020-12-27 17:51:34 +0300

    pr

--- proxies.go
+++ proxies.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/gocolly/colly"
+	"github.com/gocolly/colly/proxy"
 	"log"
+	"net"
+	"net/http"
 	"os"
+	"time"
 )
 
 func getProxies() []string {
@@ -24,4 +31,52 @@ func getProxies() []string {
 		log.Fatal(err)
 	}
 	return proxies
+}
+func checkProxies(proxies []string) []string {
+	var workProxies []string
+
+	// Rotate two socks5 proxies
+	for _, addr := range proxies {
+		c := startColly()
+		rp, err := proxy.RoundRobinProxySwitcher(addr)
+		if err != nil {
+			log.Fatal(err)
+		}
+		c.SetProxyFunc(rp)
+
+		c.OnRequest(func(r *colly.Request) {
+			//	fmt.Println("Visiting", r.URL)
+		})
+
+		c.OnResponse(func(r *colly.Response) {
+			doc, err := goquery.NewDocumentFromReader(bytes.NewReader(r.Body))
+			if err != nil {
+				log.Print(err)
+			}
+			title := doc.Find("title").Text()
+			if title == "Вы не робот?" {
+				workProxies = append(workProxies, addr)
+			}
+		})
+	}
+
+	return workProxies
+}
+
+func startColly() *colly.Collector {
+	c := colly.NewCollector(colly.AllowURLRevisit())
+	c.UserAgent = "User Agent 3.57 11.86 Mozilla/5.0 (Windows NT 6.1; rv:60.0) Gecko/20100101 Firefox/60.0"
+	c.WithTransport(&http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   60 * time.Second,
+			KeepAlive: 60 * time.Second,
+			//		DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          0,
+		IdleConnTimeout:       0,
+		TLSHandshakeTimeout:   0,
+		ExpectContinueTimeout: 0,
+	})
+	return c
 }
